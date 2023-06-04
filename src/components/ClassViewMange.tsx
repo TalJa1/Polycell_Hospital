82b89c8ae0d2fb47f187145fee0c3a703ba313ee
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2023-06-04 18:46:41 +0700

    design: pre-complete class management page

--- src/components/ClassViewMange.tsx
+++ src/components/ClassViewMange.tsx
@@ -10,16 +10,15 @@ import { RootState } from "../reduxs/Root";
 import userApi from "../api/userApi";
 // import { User } from "../models/userModel";
 import Button from "@mui/material/Button";
-// import Pagination from "@mui/material/Pagination";
-// import PaginationItem from "@mui/material/PaginationItem";
-// import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-// import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import SendIcon from "@mui/icons-material/Send";
 import Header from "./Header";
 import Footer from "./Footer";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import TextField from "@mui/material/TextField";
+import { Autocomplete } from "@mui/material";
+import { TimePicker } from "@mui/x-date-pickers";
+import ModeIcon from "@mui/icons-material/Mode";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Postdata: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -103,18 +102,97 @@ const Postdata: React.FC = () => {
           </div>
         </div>
         <div className="class-detail">
-          <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={9} md={9}></Grid>
-              <Grid
-                item
-                xs={3}
-                md={3}
-                direction="column"
-                sx={{ backgroundColor: "black", height: "100%" }}
-              ></Grid>
-            </Grid>
-          </Box>
+          <div className="column-9">
+            <div className="class-show">
+              <div className="lable-div">
+                <span>Courses</span>
+                <span>Classes</span>
+                <span>Quantities</span>
+                <span>Statuses</span>
+                <span>Trainers</span>
+                <span>Due time</span>
+                <span>Departments</span>
+                <span>Options</span>
+              </div>
+              <div className="class-info">
+                {classDetail.map((item, index) => (
+                  <div>
+                    <div
+                      className={`class-item ${
+                        index % 2 === 0 ? "even" : "odd"
+                      }`}
+                    >
+                      <span>{item.course}</span>
+                      <span>{item.class}</span>
+                      <span>{item.quantity}</span>
+                      <span>{item.status}</span>
+                      <span>{item.trainer}</span>
+                      <span className="content">{item.duetime}</span>
+                      <span>{item.deparment}</span>
+                      <span>
+                        <ModeIcon />
+                        <DeleteIcon />
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="column-3">
+            <label style={{ fontWeight: "bold", fontSize: "20px" }}>
+              Filter
+            </label>
+            <br />
+            <div className="filter-detail">
+              <label>Location</label>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                sx={{ width: "100%" }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <br />
+              <label>Type</label>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                sx={{ width: "100%" }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <br />
+              <label>Season</label>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                sx={{ width: "100%" }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <br />
+              <label>Time</label>
+              <TimePicker
+                sx={{
+                  width: "100%",
+                }}
+              />
+              <br />
+              <label>Sort by</label>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={top100Films}
+                sx={{ width: "100%" }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+              <br />
+            </div>
+            <Button variant="contained" endIcon={<SendIcon />}>
+              Send
+            </Button>
+          </div>
         </div>
       </div>
       <Footer />
@@ -124,9 +202,46 @@ const Postdata: React.FC = () => {
 
 export default Postdata;
 
+const top100Films = [
+  { label: "The Shawshank Redemption", year: 1994 },
+  { label: "The Godfather", year: 1972 },
+  { label: "The Godfather: Part II", year: 1974 },
+  { label: "The Dark Knight", year: 2008 },
+  { label: "12 Angry Men", year: 1957 },
+  { label: "Schindler's List", year: 1993 },
+  { label: "Pulp Fiction", year: 1994 },
+  {
+    label: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+  },
+];
 
-      {/* For paging */}
-      {/* <div className="pagingDiv">
+const classDetail = [
+  {
+    course: "PPG201",
+    class: "SS011",
+    quantity: "30",
+    status: "pending",
+    trainer: "Pham Van A",
+    duetime: "7am-9am (t4,t7)",
+    deparment: "A",
+  },
+  {
+    course: "PPG202",
+    class: "SS021",
+    quantity: "25",
+    status: "pending",
+    trainer: "Pham Van B",
+    duetime: "7am-9am (t2,t6)",
+    deparment: "A",
+  },
+];
+
+{
+  /* For paging */
+}
+{
+  /* <div className="pagingDiv">
         <Pagination
           onChange={handelPaging}
           page={page}
@@ -138,4 +253,5 @@ export default Postdata;
             />
           )}
         />
-      </div> */}
+      </div> */
+}
