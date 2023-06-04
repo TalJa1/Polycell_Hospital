d5c85b4c20b6d94dcf48c9e69965a44dd9de1a78
Huy <phamhuy09042002@gmail.com>
2023-06-04 19:14:32 +0700

    add add class screen

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "../styles/Home.css";
 import Menubar from "../components/Menubar";
 import Postdata from "../components/ClassManagement";
+import AddClass from "./AddClass";
 
 const home: React.FC = () => {
   return (
     <div className="container">
       <Menubar />
       <main className="rightlayout">
-        <Postdata />
+        <AddClass />
       </main>
     </div>
   );
