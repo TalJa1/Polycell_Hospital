223276e02c5aead709f7ef8944e2ccbd3e9e5b52
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2023-06-16 09:41:42 +0700

    initial folder location for trainer

---
+++ src/components/trainerComponents/TrainerHomeCp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrainerHomeCp: React.FC = () => {
+  return <div>TrainerHome</div>;
+};
+
+export default TrainerHomeCp;
---
+++ src/layouts/trainer/trainerHome.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const trainerHome: React.FC = () => {
+  return (
+    <div>trainerHome</div>
+  )
+}
+
+export default trainerHome
