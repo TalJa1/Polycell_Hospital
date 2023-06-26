836927de7cc782572a49bc27834f20756584f0be
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2023-06-26 22:16:19 +0700

    Update ParticipantsCp.tsx

--- src/components/courseComponents/ParticipantsCp.tsx
+++ src/components/courseComponents/ParticipantsCp.tsx
@@ -1,12 +1,79 @@
-import { Box } from '@mui/material';
+import { Visibility } from '@mui/icons-material';
+import { Box, IconButton } from '@mui/material';
+import { DataGrid, GridCellParams, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import React from 'react'
 
+const handleViewDetails = (row : any) => {
+  console.log(row)
+}
 const ParticipantsCp: React.FC = () => {
+
   return (
-    <Box>
-      
+    <Box sx={{
+      backgroundColor: "#ececec",
+      height: "fit-content",
+      width: "100%"
+    }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        rowSelection={false}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        // checkboxSelection
+      />
     </Box>
   )
 }
+
+//
+const columns: GridColDef[] = [
+  { field: 'name', headerName: 'Class Name', width: 130 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+  {
+    field: 'option',
+    headerName: 'Options',
+    width: 120,
+    sortable: false,
+    disableColumnMenu: true,
+    renderCell: (params: GridCellParams) => (
+      <IconButton>
+        View Details
+      </IconButton>
+    ),
+  }
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 export default ParticipantsCp;
