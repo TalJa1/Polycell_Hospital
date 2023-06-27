55e2162f53e8f48c34c6373acc3e8975c1f0067e
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2023-06-27 13:38:29 +0700

    Participant Component complex show class and trainee

--- src/components/courseComponents/ParticipantsCp.tsx
+++ src/components/courseComponents/ParticipantsCp.tsx
@@ -1,19 +1,162 @@
-import { Visibility } from '@mui/icons-material';
-import { Box, IconButton } from '@mui/material';
-import { DataGrid, GridCellParams, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import React from 'react'
+import { Box, IconButton } from "@mui/material";
+import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
+import React from "react";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Dialog from "@mui/material/Dialog";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
 
-const handleViewDetails = (row : any) => {
-  console.log(row)
-}
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="right" ref={ref} {...props} />;
+});
+
 const ParticipantsCp: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "Class Name", width: 130 },
+    { field: "createdDate", headerName: "Created date", width: 130 },
+    {
+      field: "amounts",
+      headerName: "Amounts",
+      type: "number",
+      width: 90,
+    },
+    {
+      field: "option",
+      headerName: "Options",
+      width: 120,
+      sortable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => (
+        <span>
+          {/* <Link to={``}>
+            <VisibilityIcon sx={{ color: "blue" }}/>
+          </Link> */}
+          <IconButton aria-label="view" onClick={handleClickOpen}>
+            <VisibilityIcon sx={{ color: "blue" }} />
+          </IconButton>
+        </span>
+      ),
+    },
+  ];
+
+  const columnsTrainees: GridColDef[] = [
+    { field: "id", headerName: "Trainee ID", width: 130 },
+    { field: "name", headerName: "Class Name", width: 130 },
+    { field: "email", headerName: "Email", width: 130 , sortable: false},
+    {
+      field: "phone",
+      headerName: "Phone",
+      width: 130,
+      sortable: false
+    },
+  ];
+  const rows = [
+    { id: 1, name: "LMS221", createdDate: "20/10/2022", amounts: 30 },
+    { id: 2, name: "LMS220", createdDate: "20/10/2022", amounts: 22 },
+    { id: 3, name: "LMS205", createdDate: "20/10/2022", amounts: 25 },
+    { id: 4, name: "LMS226", createdDate: "20/10/2022", amounts: 33 },
+    { id: 5, name: "LMS227", createdDate: "20/10/2022", amounts: 31 },
+    { id: 6, name: "LMS228", createdDate: "20/10/2022", amounts: 19 },
+    { id: 7, name: "LMS229", createdDate: "20/10/2022", amounts: 30 },
+    { id: 8, name: "LMS222", createdDate: "20/10/2022", amounts: 30 },
+    { id: 9, name: "LMS223", createdDate: "20/10/2022", amounts: 30 },
+    { id: 10, name: "LMS224", createdDate: "20/10/2022", amounts: 30 },
+  ];
+
+  const rowsTrainees = [
+    {
+      id: "SE161560",
+      name: "Luu Duc Hoa",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161561",
+      name: "Duong Tam",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161562",
+      name: "Thai Lan",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161563",
+      name: "Huan Nhi",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161564",
+      name: "Lien Hoa",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161565",
+      name: "Vu Hoa",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161566",
+      name: "Minh Ngoc",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161567",
+      name: "Tieu Vu",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161568",
+      name: "Chau Tan",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+    {
+      id: "SE161569",
+      name: "Chau Tan Tai",
+      email: "tai@gmail.com",
+      phone: "0909999888",
+    },
+  ];
 
   return (
-    <Box sx={{
-      backgroundColor: "#ececec",
-      height: "fit-content",
-      width: "100%"
-    }}>
+    <Box
+      sx={{
+        backgroundColor: "#ececec",
+        height: "fit-content",
+        width: "100%",
+      }}
+    >
       <DataGrid
         rows={rows}
         columns={columns}
@@ -24,56 +167,46 @@ const ParticipantsCp: React.FC = () => {
           },
         }}
         pageSizeOptions={[5, 10]}
-        // checkboxSelection
       />
+      <Dialog
+        sx={{ width: "80%", position: "right" }}
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              List trainees
+            </Typography>
+            {/* <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button> */}
+          </Toolbar>
+        </AppBar>
+        <DataGrid
+          rows={rowsTrainees}
+          columns={columnsTrainees}
+          rowSelection={false}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+        />
+      </Dialog>
     </Box>
-  )
-}
-
-//
-const columns: GridColDef[] = [
-  { field: 'name', headerName: 'Class Name', width: 130 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-  {
-    field: 'option',
-    headerName: 'Options',
-    width: 120,
-    sortable: false,
-    disableColumnMenu: true,
-    renderCell: (params: GridCellParams) => (
-      <IconButton>
-        View Details
-      </IconButton>
-    ),
-  }
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+  );
+};
 
 export default ParticipantsCp;
