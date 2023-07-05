7c39ea5bd1f13ddb2520cf39298e2b8ed7627bc1
Huy <phamhuy09042002@gmail.com>
2023-07-06 00:11:46 +0700

    change design of home page

--- src/components/courseDetaiEnrollOverview/CourseDetailEnrollRight.tsx
+++ src/components/courseDetaiEnrollOverview/CourseDetailEnrollRight.tsx
@@ -7,7 +7,7 @@ import {
   Typography,
 } from "@mui/material";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import React from "react";
 import styled from "@emotion/styled";
 
@@ -17,7 +17,70 @@ const CourseDetailEnrollRight: React.FC = () => {
       <Box
         sx={{
           border: "1px solid rgb(238, 238, 238)",
+          padding: "16px 20px",
+          marginBottom: "30px"
+        }}
+      >
+        <Typography variant="h5">Choose class to enroll</Typography>
+
+        <Box
+          sx={{
+            borderBottom: "2px solid rgb(17, 182, 122)",
+            width: "50px",
+            marginBottom: "20px",
+          }}
+        />
+
+        <FormGroup>
+          <FormControlLabel
+            sx={{
+              borderTop: "1px dashed rgb(221, 221, 221)",
+              padding: "10px 0px",
+            }}
+            control={<Checkbox />}
+            label="Class 1"
+          />
+          <FormControlLabel
+            sx={{
+              borderTop: "1px dashed rgb(221, 221, 221)",
+              padding: "10px 0px",
+            }}
+            control={<Checkbox />}
+            label="Class 1"
+          />
+          <FormControlLabel
+            sx={{
+              borderTop: "1px dashed rgb(221, 221, 221)",
+              padding: "10px 0px",
+            }}
+            control={<Checkbox />}
+            label="Class 1"
+          />
+          <FormControlLabel
+            sx={{
+              borderTop: "1px dashed rgb(221, 221, 221)",
+              padding: "10px 0px",
+            }}
+            control={<Checkbox />}
+            label="Class 1"
+          />
+        </FormGroup>
+        <GradientButton
+          sx={{
+            width: "100%",
+            // margin: "50px 0",
+            backgroundColor:
+              "linear-gradient(90deg, rgb(17, 182, 122) 0%, rgb(0, 148, 68) 100%)",
+          }}
+        >
+          Enroll
+        </GradientButton>
+      </Box>
+      <Box
+        sx={{
+          border: "1px solid rgb(238, 238, 238)",
           padding: "16px 20px ",
+          marginBottom: "30px",
         }}
       >
         <Typography variant="h5">Courses Details</Typography>
@@ -70,16 +133,6 @@ const CourseDetailEnrollRight: React.FC = () => {
             <Box>18 months</Box>
           </Box>
         </Box>
-        <GradientButton
-          sx={{
-            width: "100%",
-            // margin: "50px 0",
-            backgroundColor:
-              "linear-gradient(90deg, rgb(17, 182, 122) 0%, rgb(0, 148, 68) 100%)",
-          }}
-        >
-          Enroll
-        </GradientButton>
       </Box>
     </div>
   );
